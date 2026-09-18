9b022d5de4c96c7204784b8e275913bf205f66e1
kemelomaponyane4-wq <kemelomaponyane4@gmail.com>
2026-09-18 10:31:36 +0200

    Create PluginEditor.h

---
+++ Source/PluginEditor.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "PluginProcessor.h"
+class GuuVerbEditor : public juce::AudioProcessorEditor {
+public:
+    GuuVerbEditor(GuuVerbAudioProcessor&);
+    void paint(juce::Graphics&) override;
+    void resized() override;
+private:
+    GuuVerbAudioProcessor& p;
+};
