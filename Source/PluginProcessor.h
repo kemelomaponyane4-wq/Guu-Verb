db191b75d33685ed317a49ec175a99fcc71cd223
kemelomaponyane4-wq <kemelomaponyane4@gmail.com>
2026-09-18 11:16:55 +0200

    Update PluginProcessor.h

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,31 +1,42 @@
 #pragma once
 #include <JuceHeader.h>
 #include "FDNReverb.h"
+
 class GuuVerbAudioProcessor : public juce::AudioProcessor {
 public:
     GuuVerbAudioProcessor();
     ~GuuVerbAudioProcessor() override = default;
-    void prepareToPlay(double sr,int bs) override;
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override {}
-    bool isBusesLayoutSupported(const BusesLayout& l) const override;
-    void processBlock(juce::AudioBuffer<float>&,juce::MidiBuffer&) override;
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
+
     const juce::String getName() const override { return "GuuVerb"; }
     bool acceptsMidi() const override { return false; }
     bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
     double getTailLengthSeconds() const override { return 20.0; }
+
     int getNumPrograms() override { return 1; }
     int getCurrentProgram() override { return 0; }
     void setCurrentProgram(int) override {}
     const juce::String getProgramName(int) override { return {}; }
-    void changeProgramName(int,const juce::String&) override {}
-    void getStateInformation(juce::MemoryBlock&) override {}
-    void setStateInformation(const void*,int) override {}
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParams();
-    juce::AudioProcessorValueTreeState apvts;
+    void changeProgramName(int, const juce::String&) override {}
+
+    void getStateInformation(juce::MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts { *this, nullptr, "GuuParams", createParameterLayout() };
+
 private:
     MyFDNReverb reverb;
+    juce::AudioBuffer<float> dryBuffer;
+    float lastMix = 0.5f;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GuuVerbAudioProcessor)
 };
