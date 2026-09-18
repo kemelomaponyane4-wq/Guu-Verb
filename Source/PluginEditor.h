8b0801fd92053c25f18c0b3a02932babdad7bdc2
kemelomaponyane4-wq <kemelomaponyane4@gmail.com>
2026-09-18 11:13:14 +0200

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,10 +1,22 @@
 #pragma once
 #include "PluginProcessor.h"
+
 class GuuVerbEditor : public juce::AudioProcessorEditor {
 public:
     explicit GuuVerbEditor(GuuVerbAudioProcessor&);
+    ~GuuVerbEditor() override = default;
     void paint(juce::Graphics&) override;
-    void resized() override {}
+    void resized() override;
+
 private:
     GuuVerbAudioProcessor& proc;
+    
+    juce::Slider sizeSlider, dampSlider, mixSlider;
+    juce::Label sizeLabel, dampLabel, mixLabel;
+
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sizeAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> dampAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mixAttach;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GuuVerbEditor)
 };
