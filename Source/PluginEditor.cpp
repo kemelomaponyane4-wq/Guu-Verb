aed5df0bbff0cffaa409681ac4c61f86ced2f312
kemelomaponyane4-wq <kemelomaponyane4@gmail.com>
2026-09-18 11:01:45 +0200

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,10 +1,12 @@
 #include "PluginEditor.h"
-GuuVerbEditor::GuuVerbEditor(GuuVerbAudioProcessor& proc): AudioProcessorEditor(proc), p(proc){ setSize(500,350); }
+GuuVerbEditor::GuuVerbEditor(GuuVerbAudioProcessor& p): AudioProcessorEditor(p), proc(p){ setSize(520,360); }
 void GuuVerbEditor::paint(juce::Graphics& g){
     g.fillAll(juce::Colour(0xff6a00ff));
     g.setColour(juce::Colours::white);
-    g.setFont(28.f); g.drawText("Guu'Verb - ULTRA LONG",0,20,getWidth(),40,juce::Justification::centred);
-    g.setFont(14.f); g.drawText("Size / Damp / Mix in host",0,260,getWidth(),20,juce::Justification::centred);
+    g.setFont(juce::Font(32.0f,juce::Font::bold));
+    g.drawText("Guu'Verb - ULTRA LONG",0,30,getWidth(),40,juce::Justification::centred);
+    g.setFont(juce::Font(16.0f));
+    g.drawText("Size / Damp / Mix - Automate in DAW",0,280,getWidth(),30,juce::Justification::centred);
+    g.drawText("Tail > 10 sec - Purple Edition",0,305,getWidth(),20,juce::Justification::centred);
 }
-void GuuVerbEditor::resized(){}
 juce::AudioProcessorEditor* GuuVerbAudioProcessor::createEditor(){ return new GuuVerbEditor(*this); }
